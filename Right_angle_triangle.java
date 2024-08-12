0d0c2995fd79fc70d285f34a3ed67d180c7fe015
galichakri <galichakri517501@gmail.com>
2024-08-12 18:45:37 +0530

    Update Right_angle_triangle.java

--- Right_angle_triangle.java
+++ Right_angle_triangle.java
@@ -16,31 +16,31 @@ public class Right_angle_triangle {
 		System.out.println();
 	}
 	*/                                                    
-	/*for(i=1;i<num;i++) {                                1
-		for(j=1;j<=i;j++) {                               12
-			System.out.print(j);                          123
-		}                                                 1234
+	/*for(i=1;i<num;i++) {                                        1
+		for(j=1;j<=i;j++) {                                  12
+			System.out.print(j);                        123
+		}                                                  1234
 		System.out.println();                             12345
 	}
 	*/
 
-	/*for(i=1;i<=num;i++) {                               1
-			for(j=1;j<=i;j++) {                           22
-				System.out.print(i);                      333
+	/*for(i=1;i<=num;i++) {                                          1
+			for(j=1;j<=i;j++) {                             22
+				System.out.print(i);                   333
 			}                                             4444
-			System.out.println();                         55555
+			System.out.println();                        55555
 		}
 		*/
-	/*for(i=num;i>=1;i--) {                               *****
+	/*for(i=num;i>=1;i--) {                                   *****
 		for(j=1;j<=i;j++) {                               ****
-			System.out.print("*");                        ***
+			System.out.print("*");                    ***
 		}                                                 **
 		System.out.println();                             *
 	}
 	*/
-	for(i=num;i>=1;i--) {                 //12345
+	for(i=num;i>=1;i--) {                     //12345
 		for(j=1;j<=i;j++) {               //1234
-			System.out.print(j);          //123
+			System.out.print(j);      //123
 		}                                 //12
 		System.out.println();             //1
 	}
