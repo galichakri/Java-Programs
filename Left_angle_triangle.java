b691da14230efa7b4231673cf89067e80eb77bb6
galichakri <galichakri517501@gmail.com>
2024-08-12 18:41:47 +0530

    Update Left_angle_triangle.java

--- Left_angle_triangle.java
+++ Left_angle_triangle.java
@@ -8,27 +8,27 @@ public class Left_angle_triangle {
   	//	int num=sc.nextInt();
 		int num=5;
 		int i,j;
-		/*for(i=1;i<=num;i++) {                                    *
+		/*for(i=1;i<=num;i++) {                                        *
 			for(j=1;j<=num-i;j++) {                               **
-				System.out.print(" ");                           ***
+				System.out.print(" ");                       ***
 			}                                                   ****
 			for(j=1;j<=i;j++) {                                *****
 				System.out.print("*");
 			}
 			System.out.println();
 		}*/
-	/*	for(i=1;i<=num;i++) {                                       1
+	/*	for(i=1;i<=num;i++) {                                           1
 			for(j=1;j<=num-i;j++) {                                12
-				System.out.print(" ");                            123
+				System.out.print(" ");                        123
 			}                                                    1234
 			for(j=1;j<=i;j++) {                                 12345 
 				System.out.print(j);
 			}
 			System.out.println();
 		} */
-		for(i=1;i<=num;i++) {                                        //1
+		for(i=1;i<=num;i++) {                                            //1
 			for(j=1;j<=num-i;j++) {                                 //22
-				System.out.print(" ");                             //333
+				System.out.print(" ");                         //333
 			}                                                     //4444
 			for(j=1;j<=i;j++) {                                  //55555
 				System.out.print(i);
